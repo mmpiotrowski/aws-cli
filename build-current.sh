6ab0d23aaa16bd2d592a72a07619a8fc603236d6
Michał Piotrowski <michal@mpprojekt.org>
2023-09-02 22:53:11 +0200

    Fixes

--- build-current.sh
+++ build-current.sh
@@ -1,34 +1,44 @@
 #!/bin/bash
-set -eux
+set -eu
 
 cd $(dirname $0)
 
 REFS_TAG=$(git ls-remote --tags https://github.com/aws/aws-cli.git | awk '{print $2}' | grep /1. | grep -v '{}' | sort -rV | head -n1)
 CURRENT_AWS_CLI_V1=${REFS_TAG/"refs/tags/"/""}
 
-./build.sh $CURRENT_AWS_CLI_V1
-./push.sh $CURRENT_AWS_CLI_V1
-
 UPDATED=0
-if ! git tag -l | grep -q ${CURRENT_AWS_CLI_V1}; then
-   git tag $CURRENT_AWS_CLI_V1
-   UPDATED=1
-fi
+if  ! git tag -l | grep -q "${CURRENT_AWS_CLI_V1}"; then
+   ./build.sh $CURRENT_AWS_CLI_V1
+   ./push.sh $CURRENT_AWS_CLI_V1
 
+   
+   if ! git tag -l | grep -q ${CURRENT_AWS_CLI_V1}; then
+      git tag $CURRENT_AWS_CLI_V1
+      UPDATED=1
+   fi
+else
+   echo "Tag ${CURRENT_AWS_CLI_V1} already exits"
+fi
 
 REFS_TAG=$(git ls-remote --tags https://github.com/aws/aws-cli.git | awk '{print $2}' | grep /2. | grep -v '{}' | sort -rV | head -n1)
 CURRENT_AWS_CLI_V2=${REFS_TAG/"refs/tags/"/""}
 
-./build.sh $CURRENT_AWS_CLI_V2
-./push.sh $CURRENT_AWS_CLI_V2
+UPDATED=0
+if  ! git tag -l | grep -q "${CURRENT_AWS_CLI_V2}"; then
+   ./build.sh $CURRENT_AWS_CLI_V2
+   ./push.sh $CURRENT_AWS_CLI_V2
 
-if ! git tag -l | grep -q ${CURRENT_AWS_CLI_V2}; then
-   git tag $CURRENT_AWS_CLI_V2
-   UPDATED=1
+   if ! git tag -l | grep -q ${CURRENT_AWS_CLI_V2}; then
+      git tag $CURRENT_AWS_CLI_V2
+      UPDATED=1
+   fi
+
+else
+   echo "Tag ${CURRENT_AWS_CLI_V2} already exits"
 fi
 
 # https://joht.github.io/johtizen/build/2022/01/20/github-actions-push-into-repository.html#example-1
 if [ ${UPDATED} != 0 ]; then
-    git commit --allow-empty -m "Update aws-cli versions"
-    git push && git push --tags
+   git commit --allow-empty -m "Update aws-cli versions [skip ci]"
+   git push && git push --tags
 fi
